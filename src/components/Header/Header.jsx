57325e8cdde415906e55a08d6da32e284e599543
Stanislava Filonova <stanislavafilonova@yandex.ru>
2022-08-11 08:53:30 +0300

    Add components: Header, Techs, App, AboutProject, logotype

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// Импортируем изображение, чтобы оно отображалось на страничке
+import logotype from "../images/logotype.svg";
+
+import { useLocation } from "react-router-dom";
+import './Header.css';
+
+function Header(props) {
+
+
+}
+// Экспорт компонента, чтобы потом можно было с ним работать
+export default Header;
